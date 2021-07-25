9c61aa4064ab403cd5ec2905e2db83e68b3a002a
Danlei <45255976+danleiQ@users.noreply.github.com>
2021-07-24 23:10:10 -0400

    7.24

--- Assignments.sql
+++ Assignments.sql
@@ -1,9 +1,9 @@
---1.List of Persons’ full name, all their fax and phone numbers, as well as the phone number and fax of the company they are working for (if any). 
+--1.List of Personsâ€™ full name, all their fax and phone numbers, as well as the phone number and fax of the company they are working for (if any). 
 Select FullName,PhoneNumber, FaxNumber From Application.People 
 Union All
 Select CustomerName,PhoneNumber, FaxNumber From Sales.Customers Where CustomerCategoryID = 1
 
---2.If the customer's primary contact person has the same phone number as the customer’s phone number, list the customer companies. 
+--2.If the customer's primary contact person has the same phone number as the customerâ€™s phone number, list the customer companies. 
 Select a.CustomerName From Sales.Customers as a
 left Join Application.People as b on a.PrimaryContactPersonID = b.PersonID
 where a.PhoneNumber = b.PhoneNumber
@@ -22,9 +22,3 @@ Left Join Purchasing.PurchaseOrders c on b.PurchaseOrderID = c.PurchaseOrderID
 Where Year(OrderDate) = 2013
 Group by a.StockItemID 
 Order by StockItemID Asc
-
--- 5.List of stock items that have at least 10 characters in description.
-Select StockItemID From Warehouse.StockItems a	where LEN(SearchDetails)>10
-
--- 6.
-
